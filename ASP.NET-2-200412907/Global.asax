cb018342251738307ef7af7c632ef96598f04f75
Inderdeep Singh Chawla <200412907@student.georgianc.on.ca>
2019-05-27 17:57:02 -0400

    all

---
+++ ASP.NET-2-200412907/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASP.NET_2_200412907.MvcApplication" Language="C#" %>
---
+++ ASP.NET-2-200412907/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP.NET_2_200412907.Startup))]
+namespace ASP.NET_2_200412907
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
